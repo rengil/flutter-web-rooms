1dee1f7a4bce1bc148ebad084878e1477931acbb
Renan <renan@freework.io>
2021-05-01 16:42:47 +0200

    Try

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # Get flutter
-git clone https://github.com/flutter/flutter.git
-FLUTTER=flutter/bin/flutter
 
-# Configure flutter
-FLUTTER_CHANNEL=master
-FLUTTER_VERSION=v2.12.3
-
-# Build flutter for web
-$FLUTTER build web
 
 echo "OK"
